5d044d7c956f97ae8ea2b0821917943840fb243d
Mohammad Sami Usmani <sami.usmani48@gmail.com>
2022-10-27 01:34:07 +0500

    Add project files.

---
+++ Receive.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ClientLibrary
+{
+    public class Receive
+    {
+        public event Action<string> DataReceived;
+
+        ConnectionFactory factory = new ConnectionFactory();
+        private IModel channel;
+
+        public void SetupConnection(string hostName)
+        {
+            factory = new ConnectionFactory() { HostName = hostName };
+            var connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+        }
+
+        public void ReceiveMessageInitialize(string queue)
+        {
+            channel.QueueDeclare(queue: queue,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += OnDataReceivedFromMQ;
+            channel.BasicConsume(queue: queue,
+                                 autoAck: true,
+                                 consumer: consumer);
+        }
+
+        private void OnDataReceivedFromMQ(object sender, BasicDeliverEventArgs ea)
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            if (DataReceived != null)
+            {
+                DataReceived(message);
+            }
+        }
+    }
+}
---
+++ Send.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ClientLibrary
+{
+    internal class Send
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        private IModel channel;
+
+        public void SetupConnection(string hostName)
+        {
+            factory.HostName = hostName;
+            var connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+        }
+
+        public void SendMessage(string queue, string message)
+        {
+            try
+            {
+                channel.QueueDeclare(queue: queue,
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                             routingKey: queue,
+                                             basicProperties: null,
+                                             body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+
+                Console.WriteLine(" Press [enter] to exit.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
